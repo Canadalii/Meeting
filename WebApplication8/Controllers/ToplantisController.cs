e9faa4ace4fd775183c841a3d66fb07f3dee6240
Canadalii <adali.can.7@gmail.com>
2019-03-14 15:10:18 +0300

    proje

---
+++ WebApplication8/Controllers/ToplantisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class ToplantisController : Controller
+    {
+        private WebApplication8Context db = new WebApplication8Context();
+
+        // GET: Toplantis
+        public ActionResult Index()
+        {
+            return View(db.Toplantis.ToList());
+        }
+
+        // GET: Toplantis/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toplanti toplanti = db.Toplantis.Find(id);
+            if (toplanti == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toplanti);
+        }
+
+        // GET: Toplantis/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Toplantis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Konu,T_Tarih,BaslangicSaat,BitisSaat,Calisan,T_Katilimci")] Toplanti toplanti)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Toplantis.Add(toplanti);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(toplanti);
+        }
+
+        // GET: Toplantis/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toplanti toplanti = db.Toplantis.Find(id);
+            if (toplanti == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toplanti);
+        }
+
+        // POST: Toplantis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Konu,T_Tarih,BaslangicSaat,BitisSaat,Calisan,T_Katilimci")] Toplanti toplanti)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(toplanti).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(toplanti);
+        }
+
+        // GET: Toplantis/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Toplanti toplanti = db.Toplantis.Find(id);
+            if (toplanti == null)
+            {
+                return HttpNotFound();
+            }
+            return View(toplanti);
+        }
+
+        // POST: Toplantis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Toplanti toplanti = db.Toplantis.Find(id);
+            db.Toplantis.Remove(toplanti);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication8/Models/Toplanti.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8.Models
+{
+    public class Toplanti
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Toplantı Konusunu Belirtiniz")]
+        [Display(Name = "Toplantı Konusu")]
+        public string Konu { get; set; }
+
+
+        [Required(ErrorMessage = "Toplantı Tarihini Belirtiniz")]
+        [Display(Name = "Toplantı Tarihi")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime T_Tarih { get; set; }
+
+        [Display(Name = "Başlangıç Saati")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        [Required(ErrorMessage = "Toplantının Başlangıç Saatini Belirtiniz")]
+        public DateTime BaslangicSaat { get; set; }
+        [Required(ErrorMessage = "Toplantının Bitiş Saatini Belirtiniz")]
+        [Display(Name = "Bitiş Saati")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        public DateTime BitisSaat { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Katılan Şirket Çalışanını Belirtiniz")]
+        [Display(Name = "Katılan Çalışan")]
+        public string Calisan { get; set; }
+
+
+        [Required(ErrorMessage = "Lütfen Katılan Kişileri Belirtiniz")]
+        [Display(Name = "Katılan Kişiler")]
+        public string T_Katilimci { get; set; }
+
+
+        public class EmpDBContext : DbContext
+        {
+            public DbSet<Toplanti> Toplantilar { get; set; }
+        }
+    }
+}
